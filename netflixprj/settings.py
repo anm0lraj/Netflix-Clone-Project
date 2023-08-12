2edc43c82416cd74da632726f2cb48ad606141d2
anm0lraj <imsweetanmolraj.ar29@gmail.com>
2023-08-12 13:44:16 +0530

    postgre

--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -82,9 +82,17 @@ WSGI_APPLICATION = 'netflixprj.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'DKQ21tmZ1sQlqHc6soZ0', 
+        'HOST': 'containers-us-west-59.railway.app',
+        'PORT': '7768',
     }
 }
 
