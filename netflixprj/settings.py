3efec386b1b53d2bc828cb862d4db69d8098d74d
Anmol Raj <73774832+anm0lraj@users.noreply.github.com>
2023-08-12 13:25:05 +0530

    Update settings.py

--- netflixprj/settings.py
+++ netflixprj/settings.py
@@ -82,18 +82,18 @@ WSGI_APPLICATION = 'netflixprj.wsgi.application'
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
     # 'default': {
-    #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'railway',
-    #     'USER': 'postgres',
-    #     'PASSWORD':'uChVQyiBOeZOTSlLql1f',
-    #     'HOST': 'containers-us-west-196.railway.app',
-    #     'POST':'6625',
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD':'uChVQyiBOeZOTSlLql1f',
+        'HOST': 'containers-us-west-196.railway.app',
+        # 'POST':'6625',
+    }
 }
 
 
